4cea3e5fc7de840f8773f43bdc803e6eb111b469
alcatras <klimalakamil@gmail.com>
2017-10-10 13:32:42 +0200

    Add application launcher class.

---
+++ src/main/java/com/klimalakamil/aurora/Application.java
@@ -0,0 +1,17 @@
+package com.klimalakamil.aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kamil Klimala on 2017-10-10.
+ * klimalakamil@gmail.com
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
